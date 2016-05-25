e398a81ea2ba596bfde020b50ef3c657b5117ca8
Michel Estermann <estermann.michel@gmail.com>
2016-05-25 22:07:17 +0200

    Anagram (Elixir) iteration 2.

--- elixir/anagram/anagram.exs
+++ elixir/anagram/anagram.exs
@@ -6,17 +6,17 @@ defmodule Anagram do
   def match(base, candidates) do
     is_anagram_of_base = fn candidate -> anagrams(base, candidate) end
     candidates
-    |> Enum.reduce( [], fn (candidate, result) -> cond do
-      is_anagram_of_base.(candidate) -> result ++ [candidate]
-      true -> result
-    end end)
+    |> Enum.filter(fn (candidate) -> is_anagram_of_base.(candidate) end)
   end
 
   @spec anagrams(String.t, String.t) :: boolean
   defp anagrams(base, candidate) do
+    sorted = fn (str) -> String.to_char_list(str) |> Enum.sort end
+
     base_lowercase = String.downcase(base)
     candidate_lowercase = String.downcase(candidate)
+
     (base_lowercase != candidate_lowercase) and
-    (Enum.sort(String.to_char_list(base_lowercase)) == Enum.sort(String.to_char_list(candidate_lowercase)))
+    (sorted.(base_lowercase) == sorted.(candidate_lowercase))
   end
 end
