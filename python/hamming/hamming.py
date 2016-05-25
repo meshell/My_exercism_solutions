ea06ba18c698f52e54a0c937b43e2f77618662d0
Michel Estermann <estermann.michel@gmail.com>
2016-05-25 22:17:42 +0200

    Hamming (python) iteration 2.

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,5 +1,2 @@
 def distance(strand_1, strand_2):
-    hamming_distance = 0
-    for (n1, n2) in zip(strand_1, strand_2):
-        hamming_distance += 0 if n1 == n2 else 1
-    return hamming_distance
+    return sum([n1 != n2 for (n1, n2) in zip(strand_1, strand_2)])
