2a65f852081702e2217b9048874275cb1cee89f4
Michel Estermann <estermann.michel@gmail.com>
2016-05-25 21:34:16 +0200

    Word count (C++) iteration 3.

--- cpp/word-count/word_count.cpp
+++ cpp/word-count/word_count.cpp
@@ -1,6 +1,6 @@
 #include "word_count.h"
 #include <vector>
-#include <algorithm>
+#include <numeric>
 #include <boost/algorithm/string.hpp>
 
 namespace word_count
@@ -8,7 +8,6 @@ namespace word_count
 
 std::map<std::string, int> words(const std::string& phrase)
 {
-    std::map<std::string, int> result;
     std::vector<std::string> strings;
 
     const auto lower_case_phrase = boost::to_lower_copy(phrase);
@@ -16,21 +15,24 @@ std::map<std::string, int> words(const std::string& phrase)
                  lower_case_phrase,
                  ((not boost::is_any_of("'") && boost::is_punct()) || boost::is_space()));
 
-    for(auto& str : strings)
-    {
-        if (str.front() == '\'')
-        {
+
+    using word_count_map = std::map<std::string, int>;
+    const auto update_map_fnc = [](word_count_map result, std::string str) {
+        if (str.front() == '\'') {
             boost::erase_head(str, 1);
         }
-        if (str.back() == '\'')
-        {
+        if (str.back() == '\'') {
             boost::erase_tail(str, 1);
         }
-        if (not str.empty())
-        {
+        if (not str.empty()) {
             result[str]++;
         }
-    }
-    return result;
+        return result;
+    };
+
+    return std::accumulate(strings.cbegin(),
+                           strings.cend(),
+                           word_count_map{},
+                           update_map_fnc);
 };
 }
