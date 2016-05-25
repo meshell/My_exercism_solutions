4e7d2c0c7fc559e88dfdc17a51af7e5c5dfa4033
Michel Estermann <estermann.michel@gmail.com>
2016-05-25 21:42:23 +0200

    Hamming (C++) iteration 4.

--- cpp/hamming/hamming.cpp
+++ cpp/hamming/hamming.cpp
@@ -1,17 +1,18 @@
 #include "hamming.h"
 
-#include <algorithm>
+#include <numeric>
+#include <functional>
 #include <stdexcept>
 
 namespace hamming
 {
-unsigned compute(const std::string& sequence_1, const std::string& sequence_2)
+size_t compute(const std::string& sequence_1, const std::string& sequence_2)
 {
     if (sequence_1.size() != sequence_2.size())
     {
         throw std::domain_error("Sequences are not of same length");
     }
     return std::inner_product(sequence_1.cbegin(), sequence_1.cend(), sequence_2.cbegin(), 0,
-                              std::plus<char>(), std::not_equal_to<char>());
+                              std::plus<size_t>(), std::not_equal_to<char>());
 }
 }
