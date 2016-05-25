d6b80e501f3fc5c49c627c178a2cd19870f9f9f2
Michel Estermann <estermann.michel@gmail.com>
2016-05-25 21:26:03 +0200

    Bob (C++) Iteration 2.

--- cpp/bob/bob.cpp
+++ cpp/bob/bob.cpp
@@ -2,41 +2,51 @@
 #include <locale>
 #include <algorithm>
 
+namespace {
+bool is_silent(std::string text)
+{
+    text.erase(std::remove(std::begin(text),
+                           std::end(text), ' '),
+               std::end(text));
+    return text.empty();
+}
+
+bool is_yelling(std::string text)
+{
+    text.erase(std::remove_if(std::begin(text),
+                              std::end(text),
+                              [](const char& c){
+                                  return not isalpha(c);
+                              }),
+               std::end(text));
+    return ((not text.empty()) &&
+            (std::all_of(text.cbegin(),
+                         text.cend(),
+                         [](const char& c){
+                             return (isupper(c));
+                         })));
+}
+
+bool is_question(std::string text)
+{
+    const auto last_question_mark = text.find_last_of('?');
+    return ((last_question_mark != std::string::npos) &&
+            (last_question_mark >= text.find_last_not_of(' ')));
+}
+}
+
+
 namespace bob
 {
 std::string hey (const std::string& text)
 {
-    auto text_without_whitespace = text;
-    text_without_whitespace.erase(std::remove(std::begin(text_without_whitespace),
-                                              std::end(text_without_whitespace), ' '),
-                                  std::end(text_without_whitespace));
-    if (text_without_whitespace.empty())
-    {
+    if (is_silent(text)) {
         return "Fine. Be that way!";
     }
-
-    auto text_with_all_alpha = text_without_whitespace;
-    text_with_all_alpha.erase(std::remove_if(std::begin(text_with_all_alpha),
-                                             std::end(text_with_all_alpha),
-                                             [](const char& c){
-                                                 return not isalpha(c);
-                                             }),
-                              std::end(text_with_all_alpha));
-
-    if (not text_with_all_alpha.empty())
-    {
-        const auto is_all_upper = std::all_of(text_with_all_alpha.cbegin(),
-                                              text_with_all_alpha.cend(),
-                                              [](const char& c){
-                                                  return (isupper(c));
-                                              });
-        if (is_all_upper)
-        {
-            return "Whoa, chill out!";
-        }
+    if (is_yelling(text)) {
+        return "Whoa, chill out!";
     }
-    if (text_without_whitespace.back() == '?')
-    {
+    if (is_question(text)) {
         return "Sure.";
     }
     return "Whatever.";
