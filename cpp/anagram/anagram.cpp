ee50ea7b3c27aa11ef4197764f30ef820a696d73
Michel Estermann <estermann.michel@gmail.com>
2016-05-25 21:49:19 +0200

    Anagram (C++) iteration 3.

--- cpp/anagram/anagram.cpp
+++ cpp/anagram/anagram.cpp
@@ -13,31 +13,32 @@ bool is_anagram_of (const std::string & lhs, const std::string & rhs)
 {
     auto lhs_all_lower = boost::to_lower_copy(lhs);
     auto rhs_all_lower = boost::to_lower_copy(rhs);
-
-    std::sort(std::begin(lhs_all_lower), std::end(lhs_all_lower));
-    std::sort(std::begin(rhs_all_lower), std::end(rhs_all_lower));
-
-    return (lhs_all_lower == rhs_all_lower);
+    if ((lhs_all_lower != rhs_all_lower) &&
+        (lhs_all_lower.size() == rhs_all_lower.size()))
+    {
+        std::sort(std::begin(lhs_all_lower), std::end(lhs_all_lower));
+        std::sort(std::begin(rhs_all_lower), std::end(rhs_all_lower));
+        return (lhs_all_lower == rhs_all_lower);
+    }
+    return false;
 }
 }
 
 anagram::anagram (const std::string & str)
     : init_str{str}
 {
-
 }
 
-anagram::anagram_list_t anagram::matches (anagram::anagram_list_t possible_anagrams) const
+anagram::anagram_list_t anagram::matches (anagram::anagram_list_t canditates) const
 {
-    return std::accumulate(possible_anagrams.cbegin(),
-                           possible_anagrams.cend(),
+    return std::accumulate(canditates.cbegin(),
+                           canditates.cend(),
                            anagram_list_t{},
-                           [&] (anagram_list_t result, const std::string & possible_anagram)
+                           [&] (anagram_list_t result, const std::string & candidate)
                            {
-                               if ((boost::to_lower_copy(possible_anagram) != boost::to_lower_copy(init_str)) &&
-                                   is_anagram_of(possible_anagram, init_str))
+                               if (is_anagram_of(candidate, init_str))
                                {
-                                   result.emplace_back(possible_anagram);
+                                   result.emplace_back(candidate);
                                }
                                return result;
                            });
