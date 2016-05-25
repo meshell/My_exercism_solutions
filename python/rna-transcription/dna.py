8700f24d0bcaa19b0f0a717a8c1fb8b87daa7ccf
Michel Estermann <estermann.michel@gmail.com>
2016-05-25 22:15:10 +0200

    RNA transcription (python) iteration 2.

--- python/rna-transcription/dna.py
+++ python/rna-transcription/dna.py
@@ -1,6 +1,3 @@
 def to_rna(dna_strand):
-    dna_to_rna_transcription = dict([('A', 'U'), ('C', 'G'), ('G', 'C'), ('T', 'A')])
-    rna_strand = ''
-    for nucleotide in dna_strand:
-        rna_strand += dna_to_rna_transcription[nucleotide]
-    return rna_strand
+    dna_to_rna_transcription = {'A': 'U', 'C': 'G', 'G': 'C', 'T': 'A'}
+    return ''.join([dna_to_rna_transcription[n] for n in dna_strand])
