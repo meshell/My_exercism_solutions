5fb8d3c0452562f2f13ff81e54edf9dd811e7a81
Michel Estermann <estermann.michel@gmail.com>
2016-09-27 22:28:29 +0200

    Difference of square (C++) iteration 2.

--- cpp/difference-of-squares/difference_of_squares.h
+++ cpp/difference-of-squares/difference_of_squares.h
@@ -1,17 +1,24 @@
 #ifndef DIFFERENCE_OF_SQUARES_DIFFERENCE_OF_SQUARES_H
 #define DIFFERENCE_OF_SQUARES_DIFFERENCE_OF_SQUARES_H
 
-
 #include <cstdint>
 
 namespace squares
 {
-    uint64_t square_of_sums (uint32_t number_of_natural_numbers);
+    constexpr uint64_t square_of_sums(uint32_t n)
+    {
+        return n*n*(n+1)*(n+1)/4;
+    }
 
-    uint64_t sum_of_squares (uint32_t number_of_natural_numbers);
+    constexpr uint64_t sum_of_squares(uint32_t n)
+    {
+        return n*(n+1)*(2*n+1)/6;
+    }
 
-    uint64_t difference (uint32_t number_of_natural_numbers);
-
+    constexpr uint64_t difference(uint32_t n)
+    {
+        return square_of_sums(n) - sum_of_squares(n);
+    }
 };
 
 
