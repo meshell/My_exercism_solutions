bac510d9f05388b8fc022c64fdd3900a02608368
Michel Estermann <estermann.michel@gmail.com>
2017-10-27 22:24:05 +0200

    Sum Of Multiples (C++) iteration 2

--- cpp/sum-of-multiples/sum_of_multiples.cpp
+++ cpp/sum-of-multiples/sum_of_multiples.cpp
@@ -3,33 +3,19 @@
 #include <algorithm>
 #include <numeric>
 #include <functional>
+#include <set>
 
 namespace {
 
-template<typename T, typename Fnc>
-std::vector<T> filter(const std::vector<T>& vec, Fnc&& filter_fnc) {
-    std::vector<T> filtered;
-    std::copy_if(std::begin(vec),
-                 std::end(vec),
-                 std::back_inserter(filtered),
-                 std::forward<Fnc>(filter_fnc));
-    return filtered;
-};
-
 template<typename T>
-bool is_multiple_of_any(T value, const std::vector<T>& multiples) {
-    const auto multiple_of = [value](const T mult) {
-        return value % mult == 0;
-    };
-    return std::any_of(std::begin(multiples),
-                       std::end(multiples),
-                       multiple_of);
-}
-
-template<typename T>
-std::vector<T> generate_sequence(T limit) {
-    std::vector<T> sequence(limit);
-    std::iota(std::begin(sequence), std::end(sequence), 0);
+std::set<T> generate_multiples(std::vector<T> multiples,
+                                              T limit) {
+    std::set<T> sequence;
+    for (const auto multiple: multiples) {
+        for (auto i = multiple; i < limit; i += multiple) {
+            sequence.insert(i);
+        }
+    }
     return sequence;
 }
 
@@ -38,11 +24,7 @@ namespace sum_of_multiples {
 
 unsigned to(std::vector<unsigned> multiples, unsigned limit) {
 
-    const auto multiple_of_any = [&multiples](const auto& v) {
-        return is_multiple_of_any(v, multiples);
-    };
-    const auto all_multiples = filter(generate_sequence(limit),
-                                      multiple_of_any);
+    const auto all_multiples = generate_multiples(multiples, limit);
     return std::accumulate(std::begin(all_multiples),
                            std::end(all_multiples),
                            0,
