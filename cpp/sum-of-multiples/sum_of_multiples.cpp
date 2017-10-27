529729c40501ab4db7b6a44c1a20395b82fe1400
Michel Estermann <estermann.michel@gmail.com>
2017-10-27 23:24:36 +0200

    Sum Of Multiples (C++) iteration 3.

--- cpp/sum-of-multiples/sum_of_multiples.cpp
+++ cpp/sum-of-multiples/sum_of_multiples.cpp
@@ -3,19 +3,23 @@
 #include <algorithm>
 #include <numeric>
 #include <functional>
-#include <set>
 
 namespace {
 
 template<typename T>
-std::set<T> generate_multiples(std::vector<T> multiples,
-                                              T limit) {
-    std::set<T> sequence;
-    for (const auto multiple: multiples) {
-        for (auto i = multiple; i < limit; i += multiple) {
-            sequence.insert(i);
-        }
-    }
+bool is_multiple_of_any(T value, const std::vector<T>& multiples) {
+    const auto multiple_of = [value](const T mult) {
+        return value % mult == 0;
+    };
+    return std::any_of(std::begin(multiples),
+                       std::end(multiples),
+                       multiple_of);
+}
+
+template<typename T>
+std::vector<T> generate_sequence(T limit) {
+    std::vector<T> sequence(limit);
+    std::iota(std::begin(sequence), std::end(sequence), 0);
     return sequence;
 }
 
@@ -24,10 +28,16 @@ namespace sum_of_multiples {
 
 unsigned to(std::vector<unsigned> multiples, unsigned limit) {
 
-    const auto all_multiples = generate_multiples(multiples, limit);
-    return std::accumulate(std::begin(all_multiples),
-                           std::end(all_multiples),
-                           0,
-                           std::plus<unsigned>());
+    const auto multiple_of_any = [&multiples](const auto& v) {
+        return is_multiple_of_any(v, multiples);
+    };
+    const auto sequence = generate_sequence(limit);
+    return std::accumulate(std::begin(sequence),
+                           std::end(sequence),
+                           0u,
+                           [=](const auto& accu, const auto& v)
+                           {
+                              return multiple_of_any(v) ? accu + v: accu;
+                           });
 }
 };
